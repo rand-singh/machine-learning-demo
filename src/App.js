d80784c0c15355fbe4d17828f24eb0f37490c97b
Randeep Singh <rand-singh@users.noreply.github.com>
2021-03-11 20:14:27 +0000

    Machine learning using ml5 library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useRef, useEffect, useState } from 'react'
 import './App.css';
+import * as ml5 from 'ml5'
+import { v4 as uuidv4 } from 'uuid';
+import loading from './loading.gif'
+import tiger from './tiger.jpg';
+import zebra from './zebra.jpg';
+import parrot from './parrot.jpg';
 
 function App() {
+  const imageRef = useRef()
+  const [prediction, setPrediction] = useState([])
+
+  const classifyImg = () => {
+    const classifier = ml5.imageClassifier('MobileNet', () => console.log('model loaded'))
+
+    // Make the AI prediction
+    classifier.predict(imageRef.current, 5, (err, results) => {
+      console.log(results)
+      setPrediction(results)
+    })
+  }
+
+  useEffect(() => {
+    classifyImg()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Machine learning App!</h1>
+
+      <input type='file'/>
+      <br/>
+      <img ref={imageRef} className='candidate-image' src={zebra} alt='' crossOrigin='anonymous'/>
+      <br/>
+      <p>ml5's Artifical Intelligence image classifier model thinks this is an image of a...</p>
+      <br/>
+
+      {prediction.length === 0 ? (
+        <img className='loading-animation-image' src={loading} alt=''/>
+      ) : (
+        <ul>
+          {prediction.map(p => (
+            <li key={uuidv4()}>
+              <span><b>Label:</b> {p.label}</span>
+              <span><b>Confidence:</b> {p.confidence.toFixed(5)}%</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
