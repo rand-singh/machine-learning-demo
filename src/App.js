dd87d7cebaa614ebc2b61c2865c312a71ff9644a
Randeep Singh <rand-singh@users.noreply.github.com>
2021-03-11 21:22:02 +0000

    Add upload picture feature

--- src/App.js
+++ src/App.js
@@ -10,40 +10,54 @@ import parrot from './parrot.jpg';
 function App() {
   const imageRef = useRef()
   const [prediction, setPrediction] = useState([])
+  const [file, setFile] = useState(zebra)
 
   const classifyImg = () => {
     const classifier = ml5.imageClassifier('MobileNet', () => console.log('model loaded'))
-
     // Make the AI prediction
-    classifier.predict(imageRef.current, 5, (err, results) => {
+    classifier.predict(imageRef.current, 3, (err, results) => {
       console.log(results)
-      setPrediction(results)
+      if (results)
+        setPrediction(results)
     })
   }
 
   useEffect(() => {
     classifyImg()
-  }, [])
+  }, [file])
+
+  const fileSelectHandler = (event) => {
+    setPrediction([])
+    console.log(event)
+    let selectedFile = event.target.files[0]
+    console.log('selectedFile', selectedFile)
+    let reader = new FileReader()
+
+    reader.onload = function (event) {
+      console.log('reader.onload', event)
+      imageRef.current.src = event.target.result
+    }
+    setFile(selectedFile)
+    reader.readAsDataURL(selectedFile)
+  }
 
   return (
     <div className='app'>
       <h1>Machine learning App!</h1>
-
-      <input type='file'/>
+      <input type='file' onChange={fileSelectHandler} />
       <br/>
-      <img ref={imageRef} className='candidate-image' src={zebra} alt='' crossOrigin='anonymous'/>
+      <img ref={imageRef} className='candidate-image' src={file} alt=''/>
       <br/>
       <p>ml5's Artifical Intelligence image classifier model thinks this is an image of a...</p>
       <br/>
-
-      {prediction.length === 0 ? (
+      {prediction && prediction.length === 0 ? (
         <img className='loading-animation-image' src={loading} alt=''/>
       ) : (
         <ul>
-          {prediction.map(p => (
+          {prediction.map(({ label, confidence}) => (
             <li key={uuidv4()}>
-              <span><b>Label:</b> {p.label}</span>
-              <span><b>Confidence:</b> {p.confidence.toFixed(5)}%</span>
+              <span><b>Label:</b> {label}</span>
+              <span><b>Confidence:</b> {confidence.toFixed(5)}%</span>
             </li>
           ))}
         </ul>
